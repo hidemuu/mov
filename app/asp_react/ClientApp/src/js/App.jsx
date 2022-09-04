05b1d37f1bca57056f885dc2f59202a32ca29261
hidemuu <75873299+hidemuu@users.noreply.github.com>
2022-09-04 14:47:36 +0900

    20220904_1

--- app/asp_react/ClientApp/src/js/App.jsx
+++ app/asp_react/ClientApp/src/js/App.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { Layout } from './pages/Layout';
+import { Home } from './pages/Home';
+import { FetchData } from './pages/FetchData';
+import { Counter } from './pages/Counter';
 
-import './custom.css'
+import '../assets/css/custom.css'
 
 export default class App extends Component {
   static displayName = App.name;

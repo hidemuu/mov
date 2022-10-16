99c264efa34112794a692046e11e137c3befd0e1
hidemuu <75873299+hidemuu@users.noreply.github.com>
2022-10-16 11:41:45 +0900

    20221015_4

---
+++ src/infrastructures/wpf/wpfmvvms/ViewModels/Dialogs/WarningDialogViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Prism.Services.Dialogs;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mov.WpfMvvms.ViewModels.Dialogs
+{
+    public class WarningDialogViewModel : DialogViewModelBase
+    {
+        #region フィールド
+
+        public override string Title => "Notification";
+
+        public ReactivePropertySlim<string> Message { get; } = new ReactivePropertySlim<string>();
+
+        public string ImagePath { get; }
+
+        #endregion
+
+        #region コマンド
+
+
+
+        #endregion
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public WarningDialogViewModel()
+        {
+
+        }
+
+        #region メソッド
+
+        protected async override void OnLoaded()
+        {
+            await Task.Delay(2000);
+            RequestCloseInvoke(new DialogResult(ButtonResult.No));
+        }
+
+        public override void OnDialogOpened(IDialogParameters parameters)
+        {
+            Message.Value = parameters.GetValue<string>("message");
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/infrastructures/wpf/wpfmvvms/ViewModels/ViewModelBase.cs
@@ -0,0 +1,79 @@
+﻿using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mov.WpfMvvms.ViewModels
+{
+    /// <summary>
+    /// ビューモデル基盤クラス
+    /// </summary>
+    public abstract class ViewModelBase : BindableBase, IDisposable
+    {
+        #region フィールド
+
+        /// <summary>
+        /// ディスポーズ
+        /// </summary>
+        protected CompositeDisposable Disposables = new CompositeDisposable();
+
+        private bool _disposed;
+
+        #endregion
+
+        #region プロパティ
+
+        #endregion
+
+        #region コマンド
+
+        /// <summary>
+        /// ロード完了時コマンド
+        /// </summary>
+        public ReactiveCommand LoadedCommand { get; } = new ReactiveCommand();
+
+        #endregion
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ViewModelBase()
+        {
+            LoadedCommand.Subscribe(() => OnLoaded()).AddTo(Disposables);
+        }
+
+        #region メソッド
+
+        public void Dispose()
+        {
+            // Dispose of unmanaged resources.
+            Dispose(true);
+            // Suppress finalization.
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+            if (disposing)
+            {
+                Disposables.Dispose();
+            }
+            _disposed = true;
+        }
+
+        /// <summary>
+        /// ロード完了時処理
+        /// </summary>
+        protected virtual void OnLoaded() { }
+
+        #endregion
+
+    }
+}

9c953d34940c7074d0292d0fa1c6eec89c4f7b32
hidemuu <75873299+hidemuu@users.noreply.github.com>
2022-10-16 23:33:05 +0900

    20221016_2

---
+++ app/asp_react/ClientApp/src/ts/components/molecules/Breadcrumb/index.stories.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { ComponentMeta } from '@storybook/react'
+import Breadcrumb from './index'
+import BreadcrumbItem from '../../atoms/BreadcrumbItem'
+
+export default { title: 'Molecules/Breadcrumb' } as ComponentMeta<
+  typeof Breadcrumb
+>
+
+export const Standard = () => (
+  <Breadcrumb>
+    <BreadcrumbItem>
+      <a href="#">Top</a>
+    </BreadcrumbItem>
+    <BreadcrumbItem>
+      <a href="#">Clothes</a>
+    </BreadcrumbItem>
+    <BreadcrumbItem>Item</BreadcrumbItem>
+  </Breadcrumb>
+)

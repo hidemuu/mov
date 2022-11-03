80e8169bbe542284f6755bcd6a5307216f7714b9
hidemuu <75873299+hidemuu@users.noreply.github.com>
2022-11-03 23:15:38 +0900

    20221103_8

---
+++ src/infrastructures/wpf/wpfcontrols/Components/Atoms/MovCheckBox.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mov.WpfControls.Components.Atoms
+{
+    class MovCheckBox
+    {
+    }
+}

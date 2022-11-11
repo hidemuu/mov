8dab86d0f075f4651f8c0f67c0c914ae4894ca07
hidemuu <75873299+hidemuu@users.noreply.github.com>
2022-11-11 20:55:52 +0900

    20221111_11

---
+++ src/infrastructures/authorizers/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Authorizers
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/infrastructures/calendars/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Mov.Calendars
+{
+    public class Class1
+    {
+    }
+}

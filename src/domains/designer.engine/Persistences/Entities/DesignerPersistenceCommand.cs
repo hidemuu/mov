fee560ed669a026e0f1fdc08fecfad42816abb8f
hidemuu <75873299+hidemuu@users.noreply.github.com>
2022-12-12 00:27:43 +0900

    20221211_13

---
+++ src/domains/designer.engine/Persistences/Entities/DesignerPersistenceCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mov.Designer.Engine.Persistences.Entities
+{
+    class DesignerPersistenceCommand
+    {
+    }
+}

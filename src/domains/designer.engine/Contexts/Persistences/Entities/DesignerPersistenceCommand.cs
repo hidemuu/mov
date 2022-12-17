517fa0ac7549e5af7a9940efc506273afc05634a
hidemuu <75873299+hidemuu@users.noreply.github.com>
2022-12-17 20:46:44 +0900

    20221217_4

--- src/domains/designer.engine/Contexts/Persistences/Entities/DesignerPersistenceCommand.cs
+++ src/domains/designer.engine/Contexts/Persistences/Entities/DesignerPersistenceCommand.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Mov.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Mov.Designer.Engine.Persistences.Entities
 {
-    class DesignerPersistenceCommand
+    public class DesignerPersistenceCommand<TEntity> : IPersistenceCommand<TEntity>
     {
+        public ISave<TEntity> Saver { get; }
+
+        public IDelete<TEntity> Deleter { get; }
+
+        public DesignerPersistenceCommand()
+        {
+
+        }
+
     }
 }

7bb37afed30d8dba2822b8f992cc891c883e49c1
hidemuu <75873299+hidemuu@users.noreply.github.com>
2022-12-17 23:00:41 +0900

    20221217_6

---
+++ src/tests/commons.test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Mov.Commons.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

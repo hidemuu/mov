6ff714b446201e10c99fda8e4025438336039a41
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-03-12 00:09:15 +0900

    20230311_2

--- app/asp_react/ClientApp/src/ts/components/atoms/AppLogo/index.stories.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import { ComponentMeta } from '@storybook/react'
-import AppLogo from './index'
-
-export default { title: 'Atoms/AppLogo' } as ComponentMeta<typeof AppLogo>
-
-export const Logo = () => <AppLogo />
--- app/asp_react/ClientApp/src/ts/components/atoms/Text.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var Text = function (props) {
-    var content = props.content;
-    return (React.createElement("p", { className: "text" }, content));
-};
-exports.default = Text;
-//# sourceMappingURL=Text.js.map

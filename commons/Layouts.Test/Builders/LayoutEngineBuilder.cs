9d46bcd04ae7d2711116870bd72511def60f3dc4
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-06-26 16:03:12 +0900

    20230626

---
+++ commons/Layouts.Test/Builders/LayoutEngineBuilder.cs
@@ -0,0 +1,51 @@
+﻿using Moq;
+using Mov.Layouts;
+using Mov.Layouts.Contexts;
+using Mov.Layouts.Implements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mov.Commons.Test.Layouts.Builders
+{
+    internal class LayoutEngineBuilder
+    {
+        private readonly ILayoutEngine engine;
+        private readonly Mock<ILayoutContext> mockContext;
+
+        internal LayoutEngineBuilder()
+        {
+            this.mockContext = new Mock<ILayoutContext>();
+            this.engine = new LayoutEngine(mockContext.Object);
+        }
+
+        public LayoutEngineBuilder WithNodeCalled(IEnumerable<LayoutNode> nodes)
+        {
+            this.mockContext.Setup(x => x.Nodes).Returns(nodes);
+            return this;
+        }
+
+        public LayoutEngineBuilder WithContentCalled(IEnumerable<LayoutContent> contents)
+        {
+            this.mockContext.Setup(x => x.Contents).Returns(contents);
+            return this;
+        }
+
+        public LayoutEngineBuilder WithShellCalled(IEnumerable<LayoutShell> shells)
+        {
+            this.mockContext.Setup(x => x.Shells).Returns(shells);
+            return this;
+        }
+
+        public LayoutEngineBuilder WithThemeCalled(IEnumerable<LayoutTheme> themes)
+        {
+            this.mockContext.Setup(x => x.Themes).Returns(themes);
+            return this;
+        }
+
+        public ILayoutEngine Build() => this.engine;
+
+    }
+}
---
+++ commons/Layouts.Test/LayoutTest.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using Mov.Commons.Test.Layouts.Builders;
+using Mov.Layouts.Contexts;
+using Mov.Schemas.Layouts.Styles;
+
+namespace Mov.Commons.Test.Layouts
+{
+    public class LayoutTest
+    {
+        #region 定数
+
+        private const string TEST_NAME = "LayoutTest";
+
+        #endregion 定数
+
+        #region フィールド
+
+        private LayoutEngineBuilder builder;
+
+        #endregion フィールド
+
+        [OneTimeSetUp]
+        public void ClassInitialize()
+        {
+            this.builder = new LayoutEngineBuilder();
+            Trace.WriteLine(string.Join(' ', TEST_NAME, "ClassInitialize"));
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+
+            Trace.WriteLine(string.Join(' ', TEST_NAME, "TestInitialize"));
+        }
+
+        [TearDown]
+        public void TestCleanup()
+        {
+            Trace.WriteLine(string.Join(' ', TEST_NAME, "TestCleanup"));
+        }
+
+        [OneTimeTearDown]
+        public static void ClassCleanup()
+        {
+            Trace.WriteLine(string.Join(' ', TEST_NAME, "ClassCleanup"));
+        }
+
+        [Test]
+        public void GetCenterRegionNode()
+        {
+            //Arrange
+            var sut = this.builder
+                .WithNodeCalled(new List<LayoutNode> { LayoutNode.Empty })
+                .Build();
+
+            //Act
+            var node = sut.GetRegionNode(RegionStyle.Center);
+
+            //Assert
+            Assert.AreEqual(0, node.Children.Count);
+        }
+
+    }
+}

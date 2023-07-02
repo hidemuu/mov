b9da3349b8b1341315b785980ffc0d8ae49e6269
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-07-02 23:46:44 +0900

    20230702

--- apps/asp_react/asp_react/ClientApp/src/index.tsx
+++ apps/asp_react/asp_react/ClientApp/src/index.tsx
@@ -1,19 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') as string;
+const rootElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootElement);
 
 root.render(
-  <BrowserRouter basename={baseUrl}>
+  <Router basename={baseUrl}>
     <App />
-  </BrowserRouter>);
+  </Router>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

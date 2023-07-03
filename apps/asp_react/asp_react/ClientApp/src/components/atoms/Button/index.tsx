ab39b18eab9119043a44131fec9ab969134e9c1b
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-07-03 23:01:31 +0900

    20230703

---
+++ apps/asp_react/asp_react/ClientApp/src/components/atoms/Button/index.tsx
@@ -0,0 +1,8 @@
+﻿import * as React from "react";
+
+const Button = () =>
+{
+    return(<div></div>)
+}
+
+export default Button
--- apps/asp_react/asp_react/ClientApp/src/index.tsx
+++ apps/asp_react/asp_react/ClientApp/src/index.tsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
+import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';

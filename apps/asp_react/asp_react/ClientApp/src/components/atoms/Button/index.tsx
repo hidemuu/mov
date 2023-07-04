144ea6d9adf0c090182f4c18c0835a363fc988fc
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-07-04 23:25:56 +0900

    20230704

--- apps/asp_react/asp_react/ClientApp/src/components/atoms/Button/index.tsx
+++ apps/asp_react/asp_react/ClientApp/src/components/atoms/Button/index.tsx
@@ -1,8 +1,22 @@
 ﻿import * as React from "react";
 
-const Button = () =>
+type ButtonProps = {
+    label: string
+    text: string
+    disabled: boolean
+    onClick: React.MouseEventHandler<HTMLButtonElement>
+}
+
+const Button = (props: ButtonProps) =>
 {
-    return(<div></div>)
+    const { label, text, disabled, onClick } = props
+
+    return (
+        <div className="button-container">
+            <span>{label}</span>
+            <button disabled={disabled} onClick={onClick}>{text}</button>
+        </div>
+    )
 }
 
 export default Button

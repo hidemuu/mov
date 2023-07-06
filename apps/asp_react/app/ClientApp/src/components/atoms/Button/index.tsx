ae47838e57d84ca2edf695a55e38ed45e9f57b37
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-07-07 00:03:54 +0900

    20230706

--- apps/asp_react/app/ClientApp/src/components/atoms/Button/index.tsx
+++ apps/asp_react/app/ClientApp/src/components/atoms/Button/index.tsx
@@ -1,23 +1,4 @@
 ﻿import * as React from "react";
-import styled, { css } from "styled-components"
-
-const variants = {
-    primary: {
-        color: '#ffffff',
-        backgroundColor: '#1D3461',
-        border: 'none',
-    },
-    success: {
-        color: '#ffffff',
-        backgroundColor: '#5AB203',
-        border: 'none',
-    },
-    transparent: {
-        color: '#111111',
-        backgroundColor: 'transparent',
-        border: '1px solid black',
-    },
-} as const
 
 type ButtonProps = {
     label: string

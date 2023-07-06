34b1962dd22bd426a10b2aba77ba5501a0a34457
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-07-07 00:15:56 +0900

    20230706

---
+++ apps/asp_react/app/ClientApp/src/components/atoms/StyledButton/index.stories.tsx
@@ -0,0 +1,31 @@
+﻿import { ComponentMeta } from '@storybook/react'
+import { StyledButton } from './index'
+
+export default {
+    title: 'StyledButton',
+    component: StyledButton,
+} as ComponentMeta<typeof StyledButton>
+
+export const Primary = (props: any) => {
+    return (
+        <StyledButton {...props} variant="primary">
+            Primary
+        </StyledButton>
+    )
+}
+
+export const Success = (props: any) => {
+    return (
+        <StyledButton {...props} variant="primary">
+            Success
+        </StyledButton>
+    )
+}
+
+export const Transparent = (props: any) => {
+    return (
+        <StyledButton {...props} variant="transparent">
+            Transparent
+        </StyledButton>
+    )
+}

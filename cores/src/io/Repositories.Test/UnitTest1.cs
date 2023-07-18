1bcdeedaf408266be3284c73703b89d8c8f04ea3
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-07-19 00:32:10 +0900

    20230718

---
+++ cores/src/io/Repositories.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Repositories.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

3617a87aa5afd286b80d05a75833db0ecabc623f
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-08-12 23:43:35 +0900

    20230812

---
+++ cores/src/io/accessors.test/Models/SerializeSchemaList.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Mov.Core.Accessors.Test.Models
+{
+    [JsonArray]
+    [XmlRoot("tests")]
+    public class SerializeSchemaList : List<SerializeSchema>
+    {
+
+    }
+}

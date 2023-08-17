cbca7dac0ea2ac3aa6365198c7725a4a2282fed6
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-08-17 19:18:58 +0900

    20230817

---
+++ cores/src/io/configurators/IConfiguratorService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mov.Core.Configurators
+{
+    public interface IConfiguratorService
+    {
+    }
+}
---
+++ cores/src/io/configurators/Services/ConfiguratorService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mov.Core.Configurators.Services
+{
+    public class ConfiguratorService : IConfiguratorService
+    {
+    }
+}

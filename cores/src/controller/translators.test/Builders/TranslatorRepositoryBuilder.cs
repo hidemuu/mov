6724d21f91f461a09860a20c50bca0ad80af3228
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-09-30 22:37:08 +0900

    20230930

---
+++ cores/src/controller/translators.test/Builders/TranslatorRepositoryBuilder.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mov.Core.Translators.Test.Builders
+{
+    internal class TranslatorRepositoryBuilder
+    {
+        #region field
+
+        private readonly Mock<ITranslatorRepository> _mockRepository;
+
+
+        #endregion field
+
+        #region constructor
+
+        public TranslatorRepositoryBuilder()
+        {
+            _mockRepository = new Mock<ITranslatorRepository>();
+        }
+
+        #endregion constructor
+
+        #region method
+
+        public ITranslatorRepository Build()
+        {
+            return _mockRepository.Object;
+        }
+
+        #endregion method
+    }
+}
---
+++ cores/src/controller/translators.test/TranslatorStoreTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mov.Core.Translators.Test
+{
+    public class TranslatorStoreTest
+    {
+        #region field
+
+        #endregion field
+
+        #region setup
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        #endregion setup
+
+        #region test
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        #endregion test
+    }
+}

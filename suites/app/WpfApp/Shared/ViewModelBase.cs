aecbfdb2e6e77f38631e75b600986e4d602e86dc
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-10-02 23:18:51 +0900

    20231002

---
+++ suites/app/WpfApp/Shared/ViewModelBase.cs
@@ -0,0 +1,70 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Reactive.Disposables;
+
+namespace WpfApp.Shared
+{
+    /// <summary>
+    /// ビューモデル基盤クラス
+    /// </summary>
+    public abstract class ViewModelBase : BindableBase, IDisposable
+    {
+        #region フィールド
+
+        /// <summary>
+        /// ディスポーズ
+        /// </summary>
+        protected CompositeDisposable Disposables = new CompositeDisposable();
+
+        private bool disposed;
+
+        #endregion フィールド
+
+        #region コマンド
+
+        /// <summary>
+        /// ロード完了時コマンド
+        /// </summary>
+        public ReactiveCommand LoadedCommand { get; } = new ReactiveCommand();
+
+        #endregion コマンド
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ViewModelBase()
+        {
+            LoadedCommand.Subscribe(() => OnLoaded()).AddTo(Disposables);
+        }
+
+        #region メソッド
+
+        public void Dispose()
+        {
+            // Dispose of unmanaged resources.
+            Dispose(true);
+            // Suppress finalization.
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.disposed) return;
+            if (disposing)
+            {
+                Disposables.Dispose();
+            }
+            this.disposed = true;
+        }
+
+        /// <summary>
+        /// ロード完了時処理
+        /// </summary>
+        protected virtual void OnLoaded()
+        { }
+
+        #endregion メソッド
+    }
+}

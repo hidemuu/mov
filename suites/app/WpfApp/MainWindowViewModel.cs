7dbec6ea0a0637256a0b1312745c476f9e5ff443
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-10-08 00:16:47 +0900

    20231007

--- suites/app/WpfApp/MainWindowViewModel.cs
+++ suites/app/WpfApp/MainWindowViewModel.cs
@@ -1,18 +1,81 @@
-﻿using Mov.Suite.WpfApp.Shared;
+﻿using Mov.Suite.WpfApp.Pages.Views;
+using Mov.Suite.WpfApp.Shared;
+using Mov.Suite.WpfApp.Shared.Models;
 using Prism.Regions;
 using Prism.Services.Dialogs;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Threading;
 
 namespace Mov.Suite.WpfApp
 {
     public class MainWindowViewModel : RegionViewModelBase
     {
+        #region property
+
+        public ReactivePropertySlim<string> Title { get; private set; } = new ReactivePropertySlim<string>();
+        public ReactiveCollection<TabItemModel> TabItems { get; } = new ReactiveCollection<TabItemModel>();
+        public ReactivePropertySlim<int> TabPage { get; set; } = new ReactivePropertySlim<int>(-1);
+
+        #endregion property
+
+        #region command
+
+        public ReactiveCommand TabChangeCommand { get; } = new ReactiveCommand();
+
+        #endregion command
+
         #region constructor
 
         public MainWindowViewModel(IRegionManager regionManager, IDialogService dialogService)
             : base(regionManager, dialogService)
         {
+            TabItems.Add(new TabItemModel(0, "Game", "blur", () =>
+                this.RegionManager.RequestNavigate("CENTER", nameof(GameView))));
+
+            TabChangeCommand.Subscribe(_ => OnChangeTab()).AddTo(Disposables);
+
+            // 定期更新スレッド
+            var timer = new ReactiveTimer(TimeSpan.FromMilliseconds(10), new SynchronizationContextScheduler(SynchronizationContext.Current));
+            timer.Subscribe(_ =>
+            {
+            });
+            timer.AddTo(Disposables);
+            timer.Start();
         }
 
         #endregion constructor
+
+        #region event
+
+        protected override void OnLoaded()
+        {
+            ChangeContent(0);
+        }
+
+        private void OnChangeTab()
+        {
+            ChangeContent(TabPage.Value);
+        }
+
+        #endregion event
+
+        #region method
+
+        #endregion method
+
+        #region inner method
+
+        private void ChangeContent(int index)
+        {
+            var model = TabItems.FirstOrDefault(x => x.Index == index);
+            model?.TabCommand();
+            Title.Value = "Mov.Suite" + " - " + model.Title;
+        }
+
+        #endregion inner method
     }
 }
---
+++ suites/app/WpfApp/Shared/Models/TabItemModel.cs
@@ -0,0 +1,35 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mov.Suite.WpfApp.Shared.Models
+{
+    public class TabItemModel
+    {
+        #region property
+
+        public int Index { get; }
+        public Action TabCommand { get; }
+
+        public ReactivePropertySlim<string> Title { get; } = new ReactivePropertySlim<string>();
+        public ReactivePropertySlim<string> IconKey { get; } = new ReactivePropertySlim<string>();
+        public ReactivePropertySlim<bool> IsSelected { get; } = new ReactivePropertySlim<bool>();
+
+        #endregion property
+
+        #region constructor
+
+        public TabItemModel(int index, string title, string iconkey, Action tabcommand) 
+        {
+            Index = index;
+            TabCommand = tabcommand;
+            Title.Value = title;
+            IconKey.Value = iconkey;
+        }
+
+        #endregion constructor
+    }
+}

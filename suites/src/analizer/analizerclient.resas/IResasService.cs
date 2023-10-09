b6cc34fbd019646ee22a27b064812bfa7b3c7ea0
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-10-09 18:59:29 +0900

    20231009

--- suites/src/analizer/analizerclient.resas/IResasService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Mov.Suite.AnalizerClient.Resas
-{
-    public interface IResasService
-    {
-
-    }
-}
--- suites/src/analizer/analizerclient.resas/ResasAnalizerClient.cs
+++ suites/src/analizer/analizerclient.resas/ResasAnalizerClient.cs
@@ -1,8 +1,27 @@
-﻿using Mov.Analizer.Service;
+﻿using Mov.Analizer.Models;
+using Mov.Analizer.Service;
 
 namespace Mov.Suite.AnalizerClient.Resas
 {
     public class ResasAnalizerClient : IRegionAnalizerClient
     {
+        #region field
+
+        private readonly IAnalizerRepository _repository;
+
+        #endregion field
+
+        #region constructor
+
+        public ResasAnalizerClient(IAnalizerRepository repository) 
+        {
+            _repository = repository;
+        }
+
+        #endregion constructor
+
+        #region method
+
+        #endregion method
     }
 }

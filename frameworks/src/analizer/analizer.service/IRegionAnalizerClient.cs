693fc5515f51d12a33b6d581300d5d4658aa1689
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-10-09 18:55:49 +0900

    20231009

---
+++ frameworks/src/analizer/analizer.service/IRegionAnalizerClient.cs
@@ -0,0 +1,9 @@
+﻿namespace Mov.Analizer.Service
+{
+    /// <summary>
+    /// 地域データ分析クライアント
+    /// </summary>
+    public interface IRegionAnalizerClient
+    {
+    }
+}
---
+++ suites/src/analizer/analizerclient.resas/ResasAnalizerClient.cs
@@ -0,0 +1,8 @@
+﻿using Mov.Analizer.Service;
+
+namespace Mov.Suite.AnalizerClient.Resas
+{
+    public class ResasAnalizerClient : IRegionAnalizerClient
+    {
+    }
+}

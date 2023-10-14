9e8a5d27e9ac51a4b89a1eead628a6cf871fa9f8
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-10-14 23:29:04 +0900

    20231014

--- suites/app/ReactApp/ClientApp/src/Layout.tsx
+++ suites/app/ReactApp/ClientApp/src/Layout.tsx
@@ -81,12 +81,8 @@ export const Layout: React.FunctionComponent = theme => {
                         <div className={styles.content}>
                             <Routes>
                                 {navigationItems.map(
-                                    //item =>
-                                    //    ((item.requiresLogin && isSignedIn) || !item.requiresLogin) && (
-                                    //        <Route path={item.url} element={item.component} key={item.key} />
-                                    //    )
                                     item =>
-                                        (
+                                        ((item.requiresLogin && isSignedIn) || !item.requiresLogin) && (
                                             <Route path={item.url} element={item.component} key={item.key} />
                                         )
                                 )}

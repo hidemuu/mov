f805a04e7bb184adb28c502559fffc316b60cbb7
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-10-16 01:10:26 +0900

    20231015

--- suites/src/analizer/analizerclient.resas/ResasAnalizerClient.cs
+++ suites/src/analizer/analizerclient.resas/ResasAnalizerClient.cs
@@ -2,6 +2,7 @@
 using Mov.Analizer.Models.Schemas;
 using Mov.Analizer.Service;
 using Mov.Suite.AnalizerClient.Resas.Repository;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Mov.Suite.AnalizerClient.Resas
@@ -29,9 +30,34 @@ namespace Mov.Suite.AnalizerClient.Resas
 
         public async Task UpdateAsync()
         {
-            var cities = await _resasRepository.Cities.GetAsync(null);
+			var schemas = new HashSet<TableLineSchema>();
+			var cities = await _resasRepository.Cities.GetAsync(null);
+            foreach (var city in cities.Results)
+            {
+                schemas.Add(new TableLineSchema()
+                {
+                    Category = "city",
+                    Code = city.Code.ToString(),
+                    Name = city.Name,
+                    Content = city.Name,
+                });
+            }
 			var prefectures = await _resasRepository.Prefectures.GetAsync(null);
-            await _repository.TableLines.PostAsync(new TableLineSchema());
+			foreach (var prefecture in prefectures.Results)
+			{
+				schemas.Add(new TableLineSchema()
+				{
+					Category = "prefecture",
+					Code = prefecture.Code.ToString(),
+					Name = prefecture.Name,
+					Content = prefecture.Name,
+				});
+			}
+
+            foreach (var schema in schemas)
+            {
+				await _repository.TableLines.PostAsync(schema);
+			}
 		}
 
 		#endregion method

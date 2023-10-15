e1f27c9881355018b5642c5968a329e2645f9c7b
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-10-15 10:23:27 +0900

    20231015

--- suites/app/ReactApp/ClientApp/src/pages/ResasPage.tsx
+++ suites/app/ReactApp/ClientApp/src/pages/ResasPage.tsx
@@ -1,8 +1,88 @@
 ﻿import React, { Component, useEffect, useState } from 'react';
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import axios from "axios";
+
+const Styles: { [key: string]: React.CSSProperties } = {
+    graph: {
+        padding: "12px",
+    },
+};
+
+type Props = {
+    populationdata: {
+        prefName: string;
+        data: { year: number; value: number }[];
+    }[];
+};
 
 export const ResasPage: React.FunctionComponent = () => {
+    let series: Highcharts.SeriesOptionsType[] = [];
+    let categories = [];
+
+    const [prefectures, setPreFectures] = useState<{
+        message: null;
+        result: {
+            prefCode: number;
+            prefName: string;
+        }[];
+    } | null>(null);
+    const [prefPopulation, setPrefPopulation] = useState<
+        { prefName: string; data: { year: number; value: number }[] }[]
+    >([]);
+
+    useEffect(() => {
+        // 都道府県一覧を取得する
+        axios
+            .get('api/prefectures/', {
+                //headers: { "X-API-KEY": process.env.REACT_APP_API_KEY },
+            })
+            .then((results) => {
+                setPreFectures(results.data);
+            })
+            .catch((error) => { });
+    }, []);
+
+    for (let p of prefPopulation) {
+        let data = [];
+
+        for (let pd of p.data) {
+            data.push(pd.value);
+            categories.push(String(pd.year));
+        }
+
+        series.push({
+            type: "line",
+            name: p.prefName,
+            data: data,
+        });
+    }
+
+    const options: Highcharts.Options = {
+        title: {
+            text: "総人口推移",
+        },
+        xAxis: {
+            title: {
+                text: "年度",
+            },
+            categories: categories,
+        },
+        yAxis: {
+            title: {
+                text: "人口数",
+            },
+        },
+        // 都道府県を一つも選んでいない場合との分岐条件
+        series:
+            series.length === 0
+                ? [{ type: "line", name: "都道府県名", data: [] }]
+                : series,
+    };
+
     return (
-        <>
-        </>
+        <div style={Styles.graph}>
+            <HighchartsReact highcharts={Highcharts} options={options} />
+        </div>
     );
 };

8a2c5fe066ca2992d0ee7149ccce8c4a26a7abcc
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-10-15 12:36:51 +0900

    20231015

--- suites/app/WpfApp/Pages/ViewModels/WebViewModel.cs
+++ suites/app/WpfApp/Pages/ViewModels/WebViewModel.cs
@@ -1,8 +1,45 @@
 ﻿using Mov.Suite.WpfApp.Shared;
+using Microsoft.Web.WebView2.Core;
+using System.ComponentModel;
+using System;
 
 namespace Mov.Suite.WpfApp.Pages.ViewModels
 {
     public class WebViewModel : ViewModelBase
     {
+
+        #region property
+
+        private string webViewSource = @"https://www.example.com";
+
+        public string WebViewSource
+        {
+            get { return webViewSource; }
+            set
+            {
+                webViewSource = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("WebViewSource"));
+            }
+        }
+
+        #endregion property
+
+        #region constructor
+
+        public WebViewModel() 
+        {
+            WebViewSource = "file://" + AppDomain.CurrentDomain.BaseDirectory + "build/index.html";
+        }
+
+        #endregion constructor
+
+        #region event handler
+
+        protected override void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnPropertyChanged(args);
+        }
+
+        #endregion event hendler
     }
 }

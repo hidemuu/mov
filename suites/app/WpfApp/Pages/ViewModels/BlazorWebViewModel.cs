a89653bac5b1423566ddf96686cd9437cbcd4fd2
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-10-15 16:14:26 +0900

    20231015

---
+++ suites/app/WpfApp/Pages/ViewModels/BlazorWebViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Mov.Suite.WpfApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mov.Suite.WpfApp.Pages.ViewModels
+{
+    public class BlazorWebViewModel : ViewModelBase
+    {
+    }
+}

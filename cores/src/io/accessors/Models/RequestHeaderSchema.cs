9755230ca67a37e793105c8fc99958029b508145
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-10-20 18:33:06 +0900

    20231020

---
+++ cores/src/io/accessors/Models/RequestHeaderSchema.cs
@@ -0,0 +1,39 @@
+﻿using Mov.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mov.Core.Accessors.Models
+{
+	public sealed class RequestHeaderSchema : ValueObjectBase<RequestHeaderSchema>
+	{
+		#region property
+
+		public IReadOnlyDictionary<string, string> Headers { get; }
+
+		#endregion property
+
+		#region constructor
+
+		public RequestHeaderSchema(IReadOnlyDictionary<string, string> headers) 
+		{
+			Headers = headers;
+		}
+
+		#endregion constructor
+
+		#region method
+
+		protected override bool EqualCore(RequestHeaderSchema other)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override int GetHashCodeCore()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion method
+	}
+}

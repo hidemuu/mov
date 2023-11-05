80c1f7b07cfb83a3bacab468efb3d0df094b4f02
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-11-05 21:30:13 +0900

    20231105

---
+++ suites/app/ReactApp/Controllers/TableLineController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mov.Analizer.Service;
+
+namespace Mov.Suite.ReactApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TableLineController : ControllerBase
+	{
+		#region field
+
+		private readonly IRegionAnalizerClient _client;
+
+		#endregion field
+
+		#region constructor
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="client"></param>
+		public TableLineController(IRegionAnalizerClient client)
+		{
+			_client = client;
+		}
+
+		#endregion constructor
+
+		#region method
+
+		/// <summary>
+		/// Gets all items.
+		/// </summary>
+		[HttpGet("prefecture")]
+		public async Task<IActionResult> GetPrefectures()
+		{
+			var result = await this._client.GetTableLineAsync();
+			return Ok(result.Where(x => x.Category.Equals("prefecture")));
+		}
+
+		/// <summary>
+		/// Gets all items.
+		/// </summary>
+		[HttpGet("city")]
+		public async Task<IActionResult> GetCities()
+		{
+			var result = await this._client.GetTableLineAsync();
+			return Ok(result.Where(x => x.Category.Equals("city")));
+		}
+
+		#endregion method
+	}
+}
---
+++ suites/app/ReactApp/Controllers/TrendLineController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mov.Analizer.Service.Regions.Schemas;
+using Mov.Analizer.Service;
+using Mov.Core.Valuables;
+
+namespace Mov.Suite.ReactApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TrendLineController : ControllerBase
+	{
+		#region field
+
+		private readonly IRegionAnalizerClient _client;
+
+		#endregion field
+
+		#region constructor
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="client"></param>
+		public TrendLineController(IRegionAnalizerClient client)
+		{
+			_client = client;
+		}
+
+		#endregion constructor
+
+		#region method
+
+		/// <summary>
+		/// Gets PopulationPerYears
+		/// </summary>
+		[HttpGet("population_per_years/{prefCode}/{cityCode}")]
+		public async Task<IActionResult> GetPopulationPerYears(int prefCode, int cityCode)
+		{
+			return Ok(await this._client.GetTrendLineAsync(
+				new RegionRequestSchema()
+				{
+					PrefCode = prefCode,
+					CityCode = cityCode,
+					Category = "population_per_years",
+					Label = "all"
+				},
+				TimeValue.Empty,
+				TimeValue.Empty
+			));
+		}
+
+		#endregion method
+	}
+}

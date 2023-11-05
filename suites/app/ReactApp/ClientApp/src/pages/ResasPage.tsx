17ab9a32d9bc9ae8a2691b4c29c6f2e744aa14df
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-11-05 23:29:46 +0900

    20231105

--- suites/app/ReactApp/ClientApp/src/pages/ResasPage.tsx
+++ suites/app/ReactApp/ClientApp/src/pages/ResasPage.tsx
@@ -1,20 +1,103 @@
-﻿import React, { useEffect, useState } from 'react';
+﻿import React, { useState, useEffect } from 'react';
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import axios from "axios";
+import {
+    FolderRegular,
+    EditRegular,
+    OpenRegular,
+    DocumentRegular,
+    PeopleRegular,
+    DocumentPdfRegular,
+    VideoRegular,
+} from '@fluentui/react-icons';
+import {
+    TableBody,
+    TableCell,
+    TableRow,
+    Table,
+    TableHeader,
+    TableHeaderCell,
+    TableCellLayout,
+    PresenceBadgeStatus,
+    Avatar,
+} from '@fluentui/react-components';
+
+const items = [
+    {
+        file: { label: 'Meeting notes', icon: <DocumentRegular /> },
+        author: { label: 'Max Mustermann', status: 'available' },
+        lastUpdated: { label: '7h ago', timestamp: 1 },
+        lastUpdate: {
+            label: 'You edited this',
+            icon: <EditRegular />,
+        },
+    },
+    {
+        file: { label: 'Thursday presentation', icon: <FolderRegular /> },
+        author: { label: 'Erika Mustermann', status: 'busy' },
+        lastUpdated: { label: 'Yesterday at 1:45 PM', timestamp: 2 },
+        lastUpdate: {
+            label: 'You recently opened this',
+            icon: <OpenRegular />,
+        },
+    },
+    {
+        file: { label: 'Training recording', icon: <VideoRegular /> },
+        author: { label: 'John Doe', status: 'away' },
+        lastUpdated: { label: 'Yesterday at 1:45 PM', timestamp: 2 },
+        lastUpdate: {
+            label: 'You recently opened this',
+            icon: <OpenRegular />,
+        },
+    },
+    {
+        file: { label: 'Purchase order', icon: <DocumentPdfRegular /> },
+        author: { label: 'Jane Doe', status: 'offline' },
+        lastUpdated: { label: 'Tue at 9:30 AM', timestamp: 3 },
+        lastUpdate: {
+            label: 'You shared this in a Teams chat',
+            icon: <PeopleRegular />,
+        },
+    },
+];
+
+const columns = [
+    { columnKey: 'file', label: 'File' },
+    { columnKey: 'author', label: 'Author' },
+    { columnKey: 'lastUpdated', label: 'Last updated' },
+    { columnKey: 'lastUpdate', label: 'Last update' },
+];
 
 export const ResasPage: React.FunctionComponent = () => {
-    
+
+    const [tableLines, setTableLines] = useState<{
+        id: number;
+        category: string;
+        label: string;
+        name: string;
+        content: string;
+    }[]>([]);
+
     const [trendLines, setTrendLines] = useState<{
         id: string;
         category: string;
         label: string;
         number: number;
-        value: number
+        value: number;
     }[]>([]);
 
     useEffect(() => {
-        // 都道府県一覧を取得する
+        axios
+            .get('api/TableLine/prefecture', {
+            })
+            .then((results) => {
+                setTableLines(results.data);
+            })
+            .catch((error) => { });
+    }, []);
+
+    useEffect(() => {
         axios
             .get('api/TrendLine/population_per_years/11/11362', {
                 //headers: { "X-API-KEY": process.env.REACT_APP_API_KEY },
@@ -64,6 +147,41 @@ export const ResasPage: React.FunctionComponent = () => {
 
     return (
         <div>
+            <Table arial-label="Default table">
+                <TableHeader>
+                    <TableRow>
+                        {columns.map(column => (
+                            <TableHeaderCell key={column.columnKey}>{column.label}</TableHeaderCell>
+                        ))}
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {items.map(item => (
+                        <TableRow key={item.file.label}>
+                            <TableCell>
+                                <TableCellLayout media={item.file.icon}>{item.file.label}</TableCellLayout>
+                            </TableCell>
+                            <TableCell>
+                                <TableCellLayout
+                                    media={
+                                        <Avatar
+                                            aria-label={item.author.label}
+                                            name={item.author.label}
+                                            badge={{ status: item.author.status as PresenceBadgeStatus }}
+                                        />
+                                    }
+                                >
+                                    {item.author.label}
+                                </TableCellLayout>
+                            </TableCell>
+                            <TableCell>{item.lastUpdated.label}</TableCell>
+                            <TableCell>
+                                <TableCellLayout media={item.lastUpdate.icon}>{item.lastUpdate.label}</TableCellLayout>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
             <HighchartsReact highcharts={Highcharts} options={options} />
         </div>
     );

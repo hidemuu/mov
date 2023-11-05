62517ff88d48efecd4f825c5f316a372ffe2590a
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-11-05 15:41:26 +0900

    20231105

---
+++ suites/src/analizer/AnalizerClient.Test/Builders/ResasRepositoryBuilder.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using Mov.Suite.AnalizerClient.Resas;
+using Mov.Suite.AnalizerClient.Resas.Repository.Schemas;
+using Mov.Suite.AnalizerClient.Resas.Repository.Schemas.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mov.Suite.AnalizerClient.Test.Builders
+{
+	public class ResasRepositoryBuilder
+	{
+		#region field
+
+		private readonly Mock<IResasRepository> _mockRepository;
+
+		#endregion field
+
+		#region constructor
+
+		public ResasRepositoryBuilder() 
+		{
+			_mockRepository = new Mock<IResasRepository>();
+		}
+
+		#endregion constructor
+
+		#region method
+
+		public IResasRepository Build() => _mockRepository.Object;
+
+		public ResasRepositoryBuilder WithGetCities(ResasResponseSchema<CityResultSchema> schema)
+		{
+			_mockRepository.Setup(x => x.Cities.GetAsync(null)).ReturnsAsync(schema);
+			return this;
+		}
+
+		public ResasRepositoryBuilder WithGetPrefectures(ResasResponseSchema<PrefectureResultSchema> schema)
+		{
+			_mockRepository.Setup(x => x.Prefectures.GetAsync(null)).ReturnsAsync(schema);
+			return this;
+		}
+
+		#endregion method
+
+	}
+}
---
+++ suites/src/analizer/AnalizerClient.Test/ResasAnalizerClientTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mov.Suite.AnalizerClient.Test
+{
+	public class ResasAnalizerClientTest
+	{
+		#region constants
+
+		private const string TEST_NAME = nameof(ResasAnalizerClientTest);
+
+		#endregion constants
+
+		#region field
+
+		#endregion field
+
+		#region setup
+
+		[OneTimeSetUp]
+		public void ClassInitialize()
+		{
+			
+			Trace.WriteLine($"{TEST_NAME} ClassInitialize");
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+			Trace.WriteLine($"{TEST_NAME} TestInitialize");
+		}
+
+		[TearDown]
+		public void TestCleanup()
+		{
+			Trace.WriteLine($"{TEST_NAME} TestCleanup");
+		}
+
+		[OneTimeTearDown]
+		public static void ClassCleanup()
+		{
+			Trace.WriteLine($"{TEST_NAME} ClassCleanup");
+		}
+
+		#endregion setup
+
+		#region test
+
+		[Test]
+		public void GetTableLineAsync_MockRepository_Return()
+		{
+
+		}
+
+		#endregion test
+	}
+}

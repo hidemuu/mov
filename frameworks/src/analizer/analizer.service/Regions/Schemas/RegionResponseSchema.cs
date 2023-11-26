7b608b5ed88a9f3cc4689953d7e86d26db36abbf
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-11-26 21:56:29 +0900

    20231126

---
+++ frameworks/src/analizer/analizer.service/Regions/Schemas/RegionResponseSchema.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mov.Analizer.Service.Regions.Schemas
+{
+	public class RegionResponseSchema<T>
+	{
+		/// <summary>
+		/// カテゴリ
+		/// </summary>
+		[JsonProperty("category")]
+		public string Category { get; set; } = string.Empty;
+
+		/// <summary>
+		/// ラベル
+		/// </summary>
+		[JsonProperty("label")]
+		public string Label { get; set; } = string.Empty;
+
+		/// <summary>
+		/// データ
+		/// </summary>
+		[JsonProperty("data")]
+		public List<T> Data { get; set; } = new List<T>();
+	}
+}

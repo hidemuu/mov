88c1651849b4f24418a7598719ec41eed5553fbc
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-11-26 13:31:29 +0900

    20231126

--- suites/app/ReactApp/ClientApp/src/pages/Resas/hooks/usePopulationPerYears.ts
+++ suites/app/ReactApp/ClientApp/src/pages/Resas/hooks/usePopulationPerYears.ts
@@ -2,14 +2,26 @@ import { useState, } from 'react';
 import axios from "axios";
 import { TrendLine } from "../models/TrendLine";
 
+const API_KEY : string = 'api/TrendLine/population_per_years';
+
 export default function usePopulationPerYears(prefectureCode: string, cityCode: string) {
     console.log(prefectureCode + ' ' + cityCode);
     const [trendLines, setTrendLines] = useState<TrendLine[]>();
+    let endpoint : string;
+
+    if(cityCode === ''){
+        endpoint = API_KEY + '/' + prefectureCode;
+    }
+    else{
+        endpoint = API_KEY + '/' + prefectureCode + '/' + cityCode;
+    }
+ 
     axios
-        .get('api/TrendLine/population_per_years' + '/' + prefectureCode + '/' + cityCode, { })
+        .get(endpoint, { })
         .then((results) => {
             setTrendLines(results.data);
         })
         .catch((error) => { });
+    
     return trendLines;
 }

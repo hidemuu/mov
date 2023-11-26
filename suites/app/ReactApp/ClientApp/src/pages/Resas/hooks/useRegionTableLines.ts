f021bec005a2e0b634447a301d42f45566031abe
hidemuu <75873299+hidemuu@users.noreply.github.com>
2023-11-26 17:39:52 +0900

    20231126

--- suites/app/ReactApp/ClientApp/src/pages/Resas/hooks/useRegionTableLines.ts
+++ suites/app/ReactApp/ClientApp/src/pages/Resas/hooks/useRegionTableLines.ts
@@ -2,8 +2,9 @@ import { useState, useEffect } from 'react';
 import axios from "axios";
 import { RegionValue } from '../models/RegionValue';
 import { TableLine } from '../models/TableLine';
+import { RegionTable } from '../models/RegionTable';
 
-export default function useRegionTableLines() : [TableLine[], TableLine[]] {
+export default function useRegionTableLines() : RegionTable {
     const API_KEY_PREFECTURE : string = 'api/TableLine/prefecture';
     const API_KEY_CITY : string = 'api/TableLine/city';
     const [prefectureTableLines, setPrefectureTableLines] = useState<TableLine[]>([]);
@@ -11,20 +12,24 @@ export default function useRegionTableLines() : [TableLine[], TableLine[]] {
     
     useEffect(() =>{
         axios
-        .get(API_KEY_PREFECTURE, {
-        })
-        .then((results) => {
-            setPrefectureTableLines(results.data);
-        })
-        .catch((error) => { });
-    axios
-        .get(API_KEY_CITY, {
-        })
-        .then((results) => {
-            setCityTableLines(results.data);
-        })
-        .catch((error) => { });    
+            .get(API_KEY_PREFECTURE, {
+            })
+            .then((results) => {
+                setPrefectureTableLines(results.data);
+            })
+            .catch((error) => { });
+        axios
+            .get(API_KEY_CITY, {
+            })
+            .then((results) => {
+                setCityTableLines(results.data);
+            })
+            .catch((error) => { });    
     },[]);
 
-    return [prefectureTableLines, cityTableLines];
+    const regionTable : RegionTable = {
+        pref : prefectureTableLines,
+        city : cityTableLines,
+    }
+    return regionTable;
 }

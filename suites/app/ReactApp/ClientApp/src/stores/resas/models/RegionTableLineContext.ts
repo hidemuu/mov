1c476f7e480bc86b564a35c5d03119b854015099
hidemuu <75873299+hidemuu@users.noreply.github.com>
2024-01-05 00:35:17 +0900

    20240104

--- suites/app/ReactApp/ClientApp/src/stores/resas/models/RegionTableLineContext.ts
+++ suites/app/ReactApp/ClientApp/src/stores/resas/models/RegionTableLineContext.ts
@@ -1,5 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
 import { IRegionTableLines } from '../types/tables/IRegionTableLines'
+import { ITableLine } from '../types/tables/ITableLine';
+
+const API_KEY_PREFECTURE :string = 'api/TableLine/prefecture';
+const API_KEY_CITY :string = 'api/TableLine/city';
+
+const [prefectureTableLines, setPrefectureTableLines] = useState<ITableLine[]>([]);
+const [cityTableLines, setCityTableLines] = useState<ITableLine[]>([]);
 
 export class RegionTableLineContext {
     
@@ -12,15 +20,43 @@ export class RegionTableLineContext {
     }
 
     private constructor(){
+        this.update();
     }
 
-    private context = React.createContext<IRegionTableLines | null>(null)
+    private context :React.Context<IRegionTableLines | null> = React.createContext<IRegionTableLines | null>(null)
 
-    getContext() :React.Context<IRegionTableLines | null> {
+    public getContext() :React.Context<IRegionTableLines | null> {
         return this.context;
     }
 
-    useContext() :IRegionTableLines | null{
+    public getValue() :IRegionTableLines | null{
         return React.useContext(this.context);
+    }
+
+    private update(){
+
+        useEffect(() =>{
+            axios
+                .get(API_KEY_PREFECTURE, {
+                })
+                .then((results) => {
+                    setPrefectureTableLines(results.data);
+                })
+                .catch((error) => { });
+            axios
+                .get(API_KEY_CITY, {
+                })
+                .then((results) => {
+                    setCityTableLines(results.data);
+                })
+                .catch((error) => { });    
+        },[]);
+
+        const response : IRegionTableLines = {
+            pref : prefectureTableLines,
+            city : cityTableLines,
+        }
+
+        this.context = React.createContext<IRegionTableLines | null>(response)
     }
 }

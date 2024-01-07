a9fba4163d187b12f233c3fb1dcd5d0a7c0aa854
hidemuu <75873299+hidemuu@users.noreply.github.com>
2024-01-07 19:01:02 +0900

    20240107

--- suites/app/ReactApp/ClientApp/src/pages/Resas/ResasPage.tsx
+++ suites/app/ReactApp/ClientApp/src/pages/Resas/ResasPage.tsx
@@ -1,19 +1,14 @@
 ﻿import React, { useEffect } from 'react'
-import { Input, Label, Button } from '@fluentui/react-components'
 import type { InputProps } from '@fluentui/react-components'
 import usePopulationPerYearTrendLines from '../../stores/resas/hooks/usePopulationPerYearTrendLines'
 import useSelectedRegionState from './hooks/useSelectedRegionState'
 import { IRegionKey } from '../../stores/resas/types/IRegionKey'
-import { useStyles } from './hooks/useStyles'
 import useRegionTableLines from '../../stores/resas/hooks/useRegionTableLines'
 import { useInputId } from '../../domains/inputs/hooks/useInputId'
-import { RegionTab } from './contents/RegionTab'
-import { TrendLineChart } from './contents/TrendLineChart'
-import { RegionComboBox } from './contents/RegionComboBox'
 import { IRegionCode } from './types/IRegionCode'
+import { ResasTemplate } from '.'
 
 export const ResasPage: React.FunctionComponent = () => {
-  const styles = useStyles()
   const inputId = useInputId()
   const regionTable = useRegionTableLines()
   const initRegionCode: IRegionCode = { pref: 11, city: 11362 }
@@ -50,42 +45,19 @@ export const ResasPage: React.FunctionComponent = () => {
     }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  const onClickApply = () => {}
-
   useEffect(() => {
     //レンダリング毎に実行
     console.log('再レンダリングされるたび実行')
   })
 
   return (
-    <div className={styles.root}>
-      <div>
-        <Label htmlFor={inputId} style={{ paddingInlineEnd: '12px' }}>
-          都道府県コード
-        </Label>
-        <Input
-          id={inputId}
-          value={String(selectedRegionValue.prefCode)}
-          onChange={onChangePrefecture}
-        />
-        <RegionComboBox
-          regionValue={selectedRegionValue}
-          tableLines={regionTable}
-        />
-        <Label htmlFor={inputId} style={{ paddingInlineEnd: '12px' }}>
-          都市コード
-        </Label>
-        <Input
-          id={inputId}
-          value={String(selectedRegionValue.cityCode)}
-          onChange={onChangeCity}
-        />
-        <Button onClick={onClickApply}></Button>
-      </div>
-      <h2>トレンドグラフ</h2>
-      <TrendLineChart trendLines={populationPerYearTrendLines} />
-      <RegionTab regionTableLines={regionTable} />
-    </div>
+    <ResasTemplate
+      inputId={inputId}
+      regionTable={regionTable}
+      regionTrendLines={populationPerYearTrendLines}
+      selectedRegionKey={selectedRegionValue}
+      onChangePrefecture={onChangePrefecture}
+      onChangeCity={onChangeCity}
+    ></ResasTemplate>
   )
 }

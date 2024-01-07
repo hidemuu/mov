051922e7c76c8c37e8adaaf75dfa2956ae9c90a8
hidemuu <75873299+hidemuu@users.noreply.github.com>
2024-01-07 17:44:59 +0900

    20240107

--- suites/app/ReactApp/ClientApp/src/pages/Home/HomePage.tsx
+++ suites/app/ReactApp/ClientApp/src/pages/Home/HomePage.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { PageHeader } from '../components/organisms/PageHeader'
+import { PageHeader } from '../../components/organisms/PageHeader'
 
 export const HomePage: React.FunctionComponent = () => {
   return (

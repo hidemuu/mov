60158e734aa43a0cfc90ad3de8255a50bae503f8
hidemuu <75873299+hidemuu@users.noreply.github.com>
2024-04-13 23:58:05 +0900

    20240413

--- apps/react/mov-react/src/stores/resas/contexts/RegionItemContext.ts
+++ apps/react/mov-react/src/stores/resas/contexts/RegionItemContext.ts
@@ -1,46 +1,43 @@
-import React from "react";
+import React, {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useContext,
+} from "react";
 import { IRegionItem } from "../types/IRegionItem";
 import fetchData from "stores/resas/services/fatchData";
 
 const API_KEY = "/api/analizers/regions/resas/ResasPrefecture";
 
-export class RegionItemContext {
-  private static current: RegionItemContext;
+type RegionItemContextState = IRegionItem[];
 
-  public static get instance(): RegionItemContext {
-    if (!this.current) this.current = new RegionItemContext();
-    return this.current;
+type RegionItemContextValue = {
+  state: RegionItemContextState;
+  setState: Dispatch<SetStateAction<RegionItemContextState>>;
+};
+
+export const RegionItemContext: React.Context<
+  RegionItemContextValue | undefined
+> = createContext<RegionItemContextValue | undefined>(undefined);
+
+export function useThisContext() {
+  const value = useContext(RegionItemContext);
+  if (value === undefined)
+    throw new Error(
+      "Expected an AppProvider somewhere in the react tree to set context value"
+    );
+  return value; // now has type AppContextValue
+  // or even provide domain methods for better encapsulation
+}
+
+export function update(value: RegionItemContextValue) {
+  fetchData<IRegionItem>(API_KEY, value.setState);
+}
+
+export function asString(state: RegionItemContextState): string {
+  let result: string = "";
+  for (const item of state) {
+    result += item;
   }
-
-  private constructor() {
-    this.update();
-  }
-
-  private state: [
-    s: IRegionItem[],
-    a: React.Dispatch<React.SetStateAction<IRegionItem[]>>,
-  ] = React.useState<IRegionItem[]>([]);
-
-  public store: IRegionItem[] = this.state[0];
-
-  private context: React.Context<IRegionItem[]> = React.createContext<
-    IRegionItem[]
-  >(this.store);
-
-  public getContext(): React.Context<IRegionItem[]> {
-    return this.context;
-  }
-
-  public update() {
-    fetchData<IRegionItem>(API_KEY, this.state[1]);
-    //this.context.Provider.bind(this.regionTable)
-  }
-
-  public asString(): string {
-    let result: string = "";
-    for (const item of this.store) {
-      result += item;
-    }
-    return result;
-  }
+  return result;
 }

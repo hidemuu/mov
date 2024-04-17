eff38b662b7e9561d8acdb8d9a928278b9a57c81
hidemuu <75873299+hidemuu@users.noreply.github.com>
2024-04-18 00:08:01 +0900

    20240417

--- apps/react/mov-react/src/stores/resas/contexts/RegionTableLineContext.ts
+++ apps/react/mov-react/src/stores/resas/contexts/RegionTableLineContext.ts
@@ -1,17 +1,11 @@
-import React, {
+import {
   Dispatch,
   SetStateAction,
   createContext,
   useContext,
   useState,
 } from "react";
-import axios from "axios";
-import { IRegionTable } from "../types/tables/IRegionTable";
 import { ITableItem } from "../types/tables/ITableItem";
-
-const API_KEY = `api/TableLine`;
-const API_KEY_PREFECTURE = `${API_KEY}/prefecture`;
-const API_KEY_CITY = `${API_KEY}/city`;
 
 type RegionTableContextState = ITableItem[];
 
@@ -45,41 +39,4 @@ export function useRegionTableState(): [
   const [prefState, setPrefState] = useState<RegionTableContextState>([]);
   const [cityState, setCityState] = useState<RegionTableContextState>([]);
   return [prefState, setPrefState, cityState, setCityState];
-}
-
-export class RegionTableLineContext {
-  private static current: RegionTableLineContext;
-
-  public static get instance(): RegionTableLineContext {
-    if (!this.current) this.current = new RegionTableLineContext();
-    return this.current;
-  }
-
-  private context: React.Context<IRegionTable> =
-    React.createContext<IRegionTable>({ pref: [], city: [] });
-
-  public regionTable: IRegionTable = { pref: [], city: [] };
-
-  public getContext(): React.Context<IRegionTable> {
-    return this.context;
-  }
-
-  private updateRegionTableState() {
-    axios
-      .get(API_KEY_PREFECTURE, {})
-      .then((results) => {
-        this.regionTable.pref = results.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    axios
-      .get(API_KEY_CITY, {})
-      .then((results) => {
-        this.regionTable.city = results.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
 }

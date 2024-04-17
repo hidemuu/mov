eee21dd4028f74d7e254b5cd64c30fe9305b3874
hidemuu <75873299+hidemuu@users.noreply.github.com>
2024-04-18 00:36:44 +0900

    20240417

---
+++ cores/src/io/repositories/Schemas/Responses/DbResponseSchemaBase.cs
@@ -0,0 +1,27 @@
+﻿using CsvHelper.Configuration.Attributes;
+using FluentFTP;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Mov.Core.Repositories.Schemas.Responses
+{
+	public class DbResponseSchemaBase<TIdentifier, TResult> : DbSchemaBase<TIdentifier> where TResult : IDbResponseSchema
+	{
+		#region property
+
+		[Name("message")]
+		[JsonProperty("message")]
+		[DisplayName("メッセージ")]
+		public override TIdentifier Id { get; set; }
+
+		[Name("result")]
+		[JsonProperty("result")]
+		[DisplayName("結果")]
+		public List<TResult> Results { get; set; } = new List<TResult>();
+
+		#endregion proeprty
+	}
+}

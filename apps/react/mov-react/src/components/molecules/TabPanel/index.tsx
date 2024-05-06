1786370d8ef1695190cd24b62b0e619b5dc60e25
hidemuu <75873299+hidemuu@users.noreply.github.com>
2024-05-06 20:55:55 +0900

    20240506

---
+++ apps/react/mov-react/src/components/molecules/TabPanel/index.tsx
@@ -0,0 +1,46 @@
+import React, { FC, ReactNode, useState } from "react";
+import {
+  SelectTabData,
+  SelectTabEvent,
+  Tab,
+  TabList,
+  makeStyles,
+  shorthands,
+} from "@fluentui/react-components";
+
+const useStyles = makeStyles({
+  panels: {
+    ...shorthands.padding(0, "10px"),
+    "& th": {
+      textAlign: "left",
+      ...shorthands.padding(0, "30px", 0, 0),
+    },
+  },
+});
+
+declare type TabPanelProps = {
+  tabNames: string[];
+  components: ReactNode[];
+};
+
+export const TabPanel: FC<TabPanelProps> = ({ tabNames, components }) => {
+  const styles = useStyles();
+  const [selectedTabValue, setSelectedTabValue] = useState<number>(-1);
+
+  const onTabSelect = (event: SelectTabEvent, data: SelectTabData) => {
+    setSelectedTabValue(Number(data.value));
+  };
+
+  return (
+    <div>
+      <TabList selectedValue={selectedTabValue} onTabSelect={onTabSelect}>
+        {tabNames.map((name, index) => (
+          <Tab key={index} value={index}>
+            {name}
+          </Tab>
+        ))}
+      </TabList>
+      <div className={styles.panels}>{selectedTabValue > -1 && components[selectedTabValue]}</div>
+    </div>
+  );
+};

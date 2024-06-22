0c6d211140e1c89c3630318d93e3084dcd01c3ec
hidemuu <75873299+hidemuu@users.noreply.github.com>
2024-06-22 23:30:03 +0900

    20240622

---
+++ cores/src/controller/spreadsheets/Models/WorkBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mov.Core.SpreadSheets.Models
+{
+	public class WorkBook
+	{
+		#region property
+
+		public string Name { get; set; }
+
+		public List<WorkSheet> Sheets { get; set; } = new List<WorkSheet>();
+
+		#endregion property
+
+		#region constructor
+
+		public WorkBook(string name)
+		{
+			Name = name;	
+		}
+
+		#endregion constructor
+
+		#region method
+
+		public void Add(WorkSheet sheet)
+		{
+			Sheets.Add(sheet);
+		}
+
+		#endregion method
+	}
+}
---
+++ cores/src/controller/spreadsheets/Models/WorkTableColumn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mov.Core.SpreadSheets.Models
+{
+	public class WorkTableColumn
+	{
+		#region property
+
+		public string Name { get; set; }
+
+		public Type Type { get; set; }
+
+		#endregion property
+
+		#region constructor
+
+		public WorkTableColumn(string name, Type type)
+		{
+			Name = name;
+			Type = type;
+		}
+
+		public static class Factory 
+		{
+			public static WorkTableColumn CreateString(string name)
+			{
+				return new WorkTableColumn(name, Type.GetType("System.String"));
+			}
+
+			public static WorkTableColumn CreateInt(string name)
+			{
+				return new WorkTableColumn(name , Type.GetType("System.Int32"));
+			}
+		}
+
+		#endregion constructor
+
+	}
+}

62b14db37f766ee5f1e17d8c24f681f10e5eaff0
hidemuu <75873299+hidemuu@users.noreply.github.com>
2025-01-02 17:03:54 +0900

    20250102

---
+++ suites/TravelPlannerApp/wwwroot/src/map.js
@@ -0,0 +1,13 @@
+﻿function initializeMap() {
+    var map = L.map('map').setView([1.3521, 103.8198], 13); // シンガポールの座標
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    // サンプルのマーカー
+    L.marker([1.3521, 103.8198]).addTo(map)
+        .bindPopup('Singapore Center')
+        .openPopup();
+}

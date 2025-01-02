91cf1666948680fbd1d93a7f410b3d31588ad9cd
hidemuu <75873299+hidemuu@users.noreply.github.com>
2025-01-02 15:48:13 +0900

    20250102

--- suites/TravelPlannerApp/Program.cs
+++ suites/TravelPlannerApp/Program.cs
@@ -10,6 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<SQLiteService>();
+//builder.Services.AddScoped<SQLiteService>();
 
 await builder.Build().RunAsync();
---
+++ suites/TravelPlannerApp/wwwroot/src/scripts.js
@@ -0,0 +1,11 @@
+﻿window.addEventListener('beforeunload', () => {
+    if (window.updateLocalStorage) {
+        window.updateLocalStorage();
+    }
+});
+
+function setUpdateLocalStorage(dotNetObject) {
+    window.updateLocalStorage = function () {
+        dotNetObject.invokeMethodAsync('UpdateLocalStorage');
+    };
+}

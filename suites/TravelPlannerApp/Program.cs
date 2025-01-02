99f8e62c17f28f271f0a4b59048603c16fd24ea1
hidemuu <75873299+hidemuu@users.noreply.github.com>
2025-01-02 16:32:03 +0900

    20250102

--- suites/TravelPlannerApp/Program.cs
+++ suites/TravelPlannerApp/Program.cs
@@ -8,8 +8,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddScoped<SQLiteService>();
+var services = builder.Services;
+
+services.AddBlazoredLocalStorage();
+services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//services.AddScoped<SQLiteService>();
 
 await builder.Build().RunAsync();

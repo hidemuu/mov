5fb936d7beffcbef24bbb65d669b499dce3c5450
hidemuu <75873299+hidemuu@users.noreply.github.com>
2025-01-02 14:44:09 +0900

    20250102

---
+++ suites/TravelPlannerApp/Controllers/Apis/ItineraryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static TravelPlannerApp.Pages.Itinerary;
+
+namespace TravelPlannerApp.Controllers.Apis
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItineraryController : ControllerBase
+    {
+        private static List<DayPlan> _plans = new List<DayPlan>();
+
+        [HttpGet]
+        public IActionResult GetPlans()
+        {
+            return Ok(_plans);
+        }
+
+        [HttpPost]
+        public IActionResult SavePlans([FromBody] List<DayPlan> plans)
+        {
+            _plans = plans;
+            return Ok();
+        }
+    }
+}

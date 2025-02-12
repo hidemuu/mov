66c2c44d6e5e8f56c95cce19e426a147f29ae78f
hidemuu <75873299+hidemuu@users.noreply.github.com>
2025-02-12 11:34:43 +0900

    20250212

---
+++ suites/MoneyTrackerApp/Controllers/Models/FinanceRecord.cs
@@ -0,0 +1,13 @@
+﻿namespace MoneyTrackerApp.Controllers.Models
+{
+    public class FinanceRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public int Withdrawal { get; set; }
+        public int Deposit { get; set; }
+        public int Balance { get; set; }
+        public string Note { get; set; } = string.Empty;
+    }
+}
